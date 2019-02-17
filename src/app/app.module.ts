17793c1ff7caf451cf8614b3bc335de61b9c1d85
YoTzA <yotzazaz01@hotmail.com>
2019-02-17 10:51:48 +0700

    PP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {
+  FormsModule,
+  ReactiveFormsModule
+}from '@angular/forms'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
